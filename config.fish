5fb0e5dc6c1a3f88f5ec202371282d484dd9baa7
Arian Surya <arieyhanzz@gmail.com>
2026-01-29 06:00:00 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,7 +1,1 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+    mpv --no-video --ytdl-format="bestaudio" --term-playing-msg='Sedang Memutar: ${media-title}' "ytdl://ytsearch10:$argv"
